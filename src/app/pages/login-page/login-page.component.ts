1c85de31c66d821197f789a374cea5057cb23b2d
Maxmansq <makcmans5@mail.ru>
2025-11-26 10:05:02 +0300

    Решил вопрос с импортом но vs ругается

--- src/app/pages/login-page/login-page.component.ts
+++ src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { AuthService } from '../../auth/auth.service';
+import { AuthService } from '../..';
 import { Router } from '@angular/router';
 
 
